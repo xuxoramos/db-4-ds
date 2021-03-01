9d5b27668dae2b8c3efcd5cd8a3f54efbb5728ca
Jesus Salvador Ramos Cardona <ramos.cardona@gmail.com>
2021-03-01 12:43:44 -0600

    Update northwind_new_2021-03-01.sql

--- northwind/northwind_new_2021-03-01.sql
+++ northwind/northwind_new_2021-03-01.sql
@@ -1,25 +1,4 @@
 -- Following script has been tested in pgAdmin.
--- If you use ppScript, please following the command instruction
-
--- Connect to another default database or test database;
--- Database: testdb;
--- Disconnect existing Northwind if you have created.  
-
-DROP DATABASE IF EXISTS Northwind; 
-
-
-
-CREATE DATABASE Northwind 
-  WITH 
-    owner = hho
-    encoding = 'UTF8'
-    tablespace = pg_default 
-    lc_collate = 'en_AU.UTF-8' 
-    lc_ctype = 'en_AU.UTF-8'
-    connection LIMIT = -1 
-    template template0; 
-
-
 -- Database: Northwind
 
 DROP TABLE IF EXISTS Category;
